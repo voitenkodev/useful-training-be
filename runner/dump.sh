91ce0fd3a1945bfcf7b396a3967ee2bb5f169200
maxvoitenko <maxim.voitenko@planetvpn.dev>
2024-09-01 16:36:14 +0300

    comments

--- runner/dump.sh
+++ runner/dump.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Путь к файлу .env
+# dir .env
 ENV_FILE="../.env"
 
-# Проверка наличия файла .env
+# is founded .env
 if [ ! -f "$ENV_FILE" ]; then
   echo ".env file not found at $ENV_FILE"
   exit 1
 fi
 
-# Загрузка переменных из .env
+# load variables .env
 export $(grep -v '^#' $ENV_FILE | xargs)
 
-# Проверка обязательных переменных
+# Check variables
 if [[ -z "$POSTGRES_HOST" || -z "$POSTGRES_PORT" || -z "$POSTGRES_USERNAME" || -z "$POSTGRES_PASSWORD" || -z "$POSTGRES_DATABASE" || -z "$POSTGRES_CONTAINER_NAME" ]]; then
   echo "All environment variables must be set in .env"
   exit 1
 fi
 
-# Создание дампа базы данных
+# create dump
 docker exec -t "$POSTGRES_CONTAINER_NAME" pg_dump -U "$POSTGRES_USERNAME" -d "$POSTGRES_DATABASE" --inserts --no-owner --no-acl > dump.sql
 
 if [ $? -eq 0 ]; then
